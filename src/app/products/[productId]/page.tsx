7a0a49d7377967d86008dad5846e7482b8bbedba
Paul Lorber <paul.lorber@protonmail.com>
2024-04-24 22:47:21 +0200

    Update ProductDetails page to include metadata generation

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,25 @@
-export default function ProductDetails ({
+import { Metadata } from "next";
+
+export const generateMetadata = async ({ 
   params,
-}: {
+ }: Props): Promise <Metadata> => {
+  const title = await new Promise(resolve => {
+    setTimeout(() => {
+    resolve(`iPhone ${params.productId}`)
+  }, 100);
+});
+
+  return {
+    title: `Product ${params.productId}`,
+    description: `Product ${params.productId} details`,
+  };
+};
+
+type Props = {
   params: {productId: string};
-}) {
+};
+
+export default function ProductDetails ({params}: Props) {
   return (
     <div>
       <h1>Product Details {params.productId}</h1>
