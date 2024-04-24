fbd75b75abb6303ade6aafa5672fabd6d70d1d0b
Paul Lorber <paul.lorber@protonmail.com>
2024-04-24 23:29:11 +0200

    Update NavBar component to highlight active link in NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,24 @@
+"use client";
+
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
+import styles from './styles/NavBar.module.css';
+
 export default function NavBar() {
+  const pathname = usePathname();
   return (
     <nav>
-      <Link href="/">Home</Link>
-      <Link href="/users">Users</Link>
+      <Link 
+        href="/" 
+        className={pathname === '/' ? styles.active : ''}>
+        Home
+      </Link>
+      <Link 
+        href="/users" 
+        className={pathname === '/users' ? styles.active : ''}>
+        Users
+      </Link>
     </nav>
   );
 }
