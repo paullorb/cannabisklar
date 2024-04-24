4a574e1eaca0ebc4b1b1e5e304a60c8597fcaa0a
Paul Lorber <paul.lorber@protonmail.com>
2024-04-24 22:16:10 +0200

    Add layout.tsx component

---
+++ src/components/layout.tsx
@@ -0,0 +1,14 @@
+import NavBar from './NavBar';
+
+export default function Layout({ 
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <NavBar />
+      {children}
+    </div>
+  );
+}
