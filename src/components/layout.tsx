9f4d7c490dccdfcfb604fda564cc93f7e423d1dc
Paul Lorber <paul.lorber@protonmail.com>
2024-04-24 21:03:17 +0200

    Add layout component to provide a consistent structure for pages

---
+++ src/components/layout.tsx
@@ -0,0 +1,10 @@
+import Navbar from './NavBar';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <Navbar />
+      <main>{children}</main>
+    </div>
+  );
+}
