45cc925ffafaf15b1cafbcec2e364a89a8519e88
Paul Lorber <paul.lorber@protonmail.com>
2024-04-28 15:36:37 +0200

    Update NavBar component to highlight active link styles in NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,15 +8,15 @@ import styles from './styles/NavBar.module.css';
 export default function NavBar() {
   const pathname = usePathname();
   return (
-    <nav>
+    <nav className={styles.nav}>
       <Link 
         href="/" 
-        className={pathname === '/' ? styles.active : ''}>
+        className={pathname === '/' ? styles.active && styles.Link : styles.Link}>
         Home
       </Link>
       <Link 
         href="/users" 
-        className={pathname === '/users' ? styles.active : ''}>
+        className={pathname === '/users' ? styles.active && styles.Link : styles.Link}>
         Users
       </Link>
     </nav>
