9b164990239fee60de2be1aebff2228701cb1ae7
Paul Lorber <paul.lorber@protonmail.com>
2024-04-28 16:07:53 +0200

    Refactor ErrorBoundary component to handle errors with error and reset props

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,5 +1,11 @@
 'use client';
 
-export default function ErrorBoundary () {
-  return <div>Error in reviewId</div>
+export default function ErrorBoundary ({
+  error, 
+  reset
+}: {
+  error: Error; 
+  reset: () => void
+}) {
+  return <div>{error.message}<button onClick={reset}>Try again</button></div>
 }
