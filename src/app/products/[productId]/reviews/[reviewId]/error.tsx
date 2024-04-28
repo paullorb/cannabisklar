ce1b27c41eeca6e287655cb50cabf43a8de8d086
Paul Lorber <paul.lorber@protonmail.com>
2024-04-28 15:30:13 +0200

    Add ErrorBoundary component to handle errors in reviews page

---
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react';
+
+export default function ErrorBoundary ({error}: {error: Error}) {
+  return (
+    <div>
+      <h1>{error.message}</h1>
+    </div>
+  );
+}
