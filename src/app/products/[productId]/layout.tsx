e80920cccb22f1cbf1635183eecb11ac85c31de6
Paul Lorber <paul.lorber@protonmail.com>
2024-04-28 19:32:16 +0200

    Fix error handling in ProductDetailsLayout component

--- src/app/products/[productId]/layout.tsx
+++ src/app/products/[productId]/layout.tsx
@@ -1,12 +1,23 @@
+function getRandomInt (count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default function ProductDetailsLayout ({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  const random = getRandomInt(2);
+
+  if(random === 1) {
+    throw new Error("Error loading product");
+  }
+
   return (
     <div>
       {children}
-      <h1>Product Features</h1>
+      <h1>Features products</h1>
     </div>
   );
 }
