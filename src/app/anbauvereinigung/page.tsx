94d9a6583f1da01bf1da7e0d755297987bab1fb4
Paul Lorber <paul.lorber@protonmail.com>
2024-05-16 10:53:45 +0200

    changes

--- src/app/anbauvereinigung/page.tsx
+++ src/app/anbauvereinigung/page.tsx
@@ -1,6 +1,8 @@
-export default function Unternehmen () {
+import styles from './Anbauvereinigung.module.css';
+
+export default function Anbauvereinigungen () {
   return (
-    <div>
+    <section>
       <h1>Unternehmen</h1>
     <ul>
       <li>Die Einfuhr von Cannabis aus dem Ausland nach Deutschland bleibt hingegen verboten.</li>
@@ -10,6 +12,15 @@ export default function Unternehmen () {
     <ul>
       <li>Minderjährige dürfen Anbauvereinigungen nicht beitreten. An 18- bis 21-jährige Mitglieder darf in Anbauvereinigungen nur Cannabis mit einem begrenzten THC-Gehalt weitergegeben werden.</li>
     </ul>
+    <div className={styles.comparison}>
+      <div className={styles.card}>
+        <h1 className={styles.heading}>Verein</h1>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.heading}>Genossenschaft</h1>
+          <p>ein Zusammenschluss mehrerer natürlicher oder juristischer Personen, deren Ziel die gegenseitige Förderung durch gemeinsame wirtschaftliche Tätigkeit ist.</p>
+          </div>
     </div>
+    </section>
   );
 }
--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -7,8 +7,14 @@ export default function Feedback() {
     <div className={styles.container}>
       <h2 className={styles.heading}>Feedback</h2>
       <div className={styles.icons}>
-      <Image src="/face-happy.svg" alt="happy face" width="25" height="25"/>
-      <Image src="/face-sad.svg" alt="sad face" width="25" height="25"/>
+        <div className={styles.happy}>
+          <Image className={styles.outline} src="/face-happy.svg" alt="happy face" width="25" height="25"/>
+          <Image className={styles.solid} src="/face-happy-filled.svg" alt="happy face filled" width="25" height="25"/>
+        </div>
+        <div className={styles.sad}>
+          <Image className={styles.outline} src="/face-sad.svg" alt="sad face" width="25" height="25"/>
+          <Image className={styles.solid} src="/face-sad-filled.svg" alt="sad face filled" width="25" height="25"/>
+        </div>
       </div>
     </div>
   )}
